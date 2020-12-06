7af8b4ebc563a5fc9162cb0425cb914a86decd0b
Carlos Jiménez <carlosjimz87@gmail.com>
2020-12-06 18:50:35 +0100

    model Aircraft and Flight done & tested

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields import CharField
+from django.conf import settings
 
-# Create your models here.
+
+def get_airport_codes():
+    with open(settings.MEDIA_ROOT / "airports") as f:
+        return [[line, line] for line in f.read().splitlines()]
+
+
+class Aircraft(models.Model):
+    serial_number = models.CharField(max_length=6)
+    manufacturer = models.CharField(max_length=20)
+
+    class Meta:
+        ordering = ['manufacturer']
+
+
+class Flight(models.Model):
+    departure = models.CharField(choices=get_airport_codes(), max_length=4)
+    arrival = models.CharField(choices=get_airport_codes(), max_length=4)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    aircraft = models.ForeignKey(
+        Aircraft, related_name='flights', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['departure_time', 'arrival_time']
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from .models import Aircraft, Flight
+from django.utils.timezone import now
+from datetime import timedelta
 
-# Create your tests here.
+
+class Models(TestCase):
+    def setUp(self) -> None:
+        self.aircraft_defaults = [
+            dict(serial_number="AA5435", manufacturer="Airbus"),
+            dict(serial_number="BA2311", manufacturer="Boeing"),
+            dict(serial_number="EM8799", manufacturer="Embraer"),
+        ]
+        aircraft_objects = [Aircraft.objects.create(
+            **aircraft) for aircraft in self.aircraft_defaults]
+
+        self.flight_defaults = [
+            dict(departure="00AA",
+                 arrival="00AK",
+                 departure_time=now(),
+                 arrival_time=now()+timedelta(hours=3),
+                 aircraft=aircraft_objects[0]
+                 ),
+            dict(departure="KCGF",
+                 arrival="KCHQ",
+                 departure_time=now(),
+                 arrival_time=now()+timedelta(hours=6),
+                 aircraft=aircraft_objects[1]
+                 ),
+            dict(departure="SVBW",
+                 arrival="SVCO",
+                 departure_time=now(),
+                 arrival_time=now()+timedelta(hours=8),
+                 aircraft=aircraft_objects[2]
+                 ),
+        ]
+
+        [Flight.objects.create(
+            **flight) for flight in self.flight_defaults]
+
+    def test_aircrafts(self):
+        aircrafts = Aircraft.objects.all()
+        self.assertEqual(len(aircrafts), 3)
+
+        for pair in zip(self.aircraft_defaults, aircrafts):
+            self.assertEqual(pair[0]["serial_number"], pair[1].serial_number)
+            self.assertEqual(pair[0]["manufacturer"], pair[1].manufacturer)
+
+    def test_flights(self):
+        flights = Flight.objects.all()
+        self.assertEqual(len(flights), 3)
+
+        for pair in zip(self.flight_defaults, flights):
+            self.assertEqual(pair[0]["departure"], pair[1].departure)
+            self.assertEqual(pair[0]["arrival"], pair[1].arrival)
+            self.assertEqual(pair[0]["departure_time"], pair[1].departure_time)
+            self.assertEqual(pair[0]["arrival_time"], pair[1].arrival_time)
+            self.assertEqual(pair[0]["aircraft"], pair[1].aircraft)
